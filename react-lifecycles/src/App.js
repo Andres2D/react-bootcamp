0f5d9bc7810428003f634bd7118ca94646e9cc85
Andres2D <andres2d1997.1206@gmail.com>
2022-05-05 14:22:34 -0500

    Add lifecycle project base

---
+++ react-lifecycles/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Timer from './Basics/Timer';
+import ZenQuote from './Basics/ZenQuote';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Timer /> */}
+      <ZenQuote />
+    </div>
+  );
+}
+
+export default App;
