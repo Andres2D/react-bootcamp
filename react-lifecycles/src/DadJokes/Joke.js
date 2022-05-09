447747e3c812d7a407cf428537d7466a2a13e8e9
Andres2D <andres2d1997.1206@gmail.com>
2022-05-09 17:30:58 -0500

    Finish dad jokes app

--- react-lifecycles/src/DadJokes/Joke.js
+++ react-lifecycles/src/DadJokes/Joke.js
@@ -3,23 +3,63 @@ import './Joke.css';
 
 class Joke extends Component {
 
-    render() {
-        return (
-            <div>
-                <div className='joke'>
-                    <div className='vote'>
-                        <button onClick={this.props.upvote} className='vote-arrow'>⬆</button>
-                        <span className='score'>{this.props.vote}</span>
-                        <button onClick={this.props.downvote} className='vote-arrow'>⬇</button>
-                    </div>
-                    <div className='text'>{this.props.joke}</div>
-                    <div className='emoji-change'> :D </div>
-                </div>
-                <hr />
-            </div>
-        )
+  getColor() {
+    if(this.props.vote >= 15) {
+      return '#4CAF50';
+    }else if(this.props.vote >= 12) {
+      return '#8BC34A'; 
+    }else if(this.props.vote >= 9) {
+      return '#CDDC39'; 
+    }else if(this.props.vote >= 6) {
+      return '#FFEB3B';
+    }else if(this.props.vote >= 3) {
+      return '#FFC107';
+    }else if(this.props.vote >= 0) {
+      return '#FF9800';
+    }else {
+      return '#F44336';
     }
+  }
 
+  getEmojiClass() {
+    if(this.props.vote >= 15) {
+      return 'em em-rolling_on_the_floor_laughing';
+    }else if(this.props.vote >= 12) {
+      return 'em em-laughing'; 
+    }else if(this.props.vote >= 9) {
+      return 'em em-smiley'; 
+    }else if(this.props.vote >= 6) {
+      return 'em em-slightly_smiling_face';
+    }else if(this.props.vote >= 3) {
+      return 'em em-neutral_face';
+    }else if(this.props.vote >= 0) {
+      return 'em em-confused';
+    }else {
+      return 'em em-angry';
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='joke'>
+          <div className='vote'>
+            <button onClick={this.props.upvote} className='vote-arrow'>
+              <i className="em em-arrow_up" aria-label="UPWARDS BLACK ARROW"></i>
+            </button>
+            <span className='score' style={{borderColor: this.getColor()}}>{this.props.vote}</span>
+            <button onClick={this.props.downvote} className='vote-arrow'>
+              <i className="em em-arrow_down" aria-label="DOWNWARDS BLACK ARROW"></i>
+            </button>
+          </div>
+          <div className='text'>{this.props.joke}</div>
+          <div className='emoji-change'>
+            <i className={this.getEmojiClass()} aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Joke;
--- react-lifecycles/src/DadJokes/Jokes.js
+++ react-lifecycles/src/DadJokes/Jokes.js
@@ -8,62 +8,113 @@ const URL_BASE = 'https://icanhazdadjoke.com/';
 
 class Jokes extends Component {
 
-    static defaultProps = {
-        jokesLength: 10
-    }
+  static defaultProps = {
+    jokesLength: 10
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            jokes: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: JSON.parse(localStorage.getItem('jokes')) || [],
+      loading: true
+    }
+    this.seenJokes = new Set(this.state.jokes.map(j => j.text));
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.getJokes();
+  }
+
+  handleVotes(id, delta) {
+    this.setState(st => ({
+      jokes: st.jokes.map(j => j.id === id ? { ...j, vote: j.vote + delta } : j)
+    }),
+    () => this.saveToLocalStorage(this.state.jokes));
+  }
+  
+  componentDidMount() {
+    if(this.state.jokes.length === 0) {
+      this.getJokes();
+    }else {
+      this.setState({
+        loading: false
+      })
+    }
+  }
+
+  async getJokes() {
+    try {
+      this.setState({
+        loading: true
+      });
+  
+      let jokes = [];
+      while (jokes.length < this.props.jokesLength) {
+        const response = await axios.get(URL_BASE, {
+          headers: {
+            Accept: 'application/json'
+          }
+        });
+        const newJoke = response.data.joke; 
+        if(!this.seenJokes.has(newJoke))
+        {
+          jokes.push({ joke: newJoke , vote: 0, id: uuidv4() });
+        } else{
+          console.log('Duplicated');
         }
+          
+      }
+      this.setState(st => ({
+        jokes: [...st.jokes, ...jokes],
+        loading: false
+      }),
+      () => this.saveToLocalStorage(this.state.jokes));  
+    } catch(err) {
+      alert(err);
+      this.setState({
+        loading: false
+      })
     }
+  }
 
-    handleVotes(id, delta) {
-        console.log(id, delta);
-        this.setState(st => ({
-            jokes: st.jokes.map(j => j.id === id ? {...j, vote: j.vote + delta} : j)
-        }));
-    }
+  saveToLocalStorage(jokes) {
+    const currentJokes = JSON.stringify(jokes);
+    localStorage.setItem('jokes', currentJokes);
+  }
 
-    async componentDidMount() {
-        let jokes = [];
-        while(jokes.length < this.props.jokesLength) {
-            const response = await axios.get(URL_BASE, {
-                headers: {
-                    Accept: 'application/json'
-                }
-            });
-            jokes.push({joke: response.data.joke, vote: 0, id: uuidv4()});
-        }
+  render() {
+    const jokes = this.state.jokes.sort((a,b) => b.vote - a.vote);
+    const jokesRender = jokes.map((j) => (
+      <Joke
+        key={j.id}
+        vote={j.vote}
+        joke={j.joke}
+        upvote={() => this.handleVotes(j.id, 1)}
+        downvote={() => this.handleVotes(j.id, -1)} />)
+    );
 
-        this.setState({
-            jokes
-        })
-    }
+    const loader = (
+      <div className="loader">
+        <h3>Loading</h3>  
+      </div>
+    );
 
-    render() {
-        const jokesRender = this.state.jokes.map(j => (
-            <Joke 
-                key={j.id} 
-                vote={j.vote} 
-                joke={j.joke}
-                upvote={() => this.handleVotes(j.id, 1)}
-                downvote={() => this.handleVotes(j.id, -1)} />)
-        )
-        return (
-            <div className="jokes-list">
-                <div className='sidebar'>
-                    <h1>Dad<span className="bold-title"> JOKES</span></h1>
-                    <img className='emoji' src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt='Laugh'/>
-                    <button className='button'><h1>New Jokes</h1></button>  
-                </div>
-                <div className="jokes">
-                    {jokesRender}
-                </div>
-            </div>
-        )
-    }
+    const application = (
+      <div className="jokes-list">
+        <div className='sidebar'>
+          <h1>Dad<span className="bold-title"> JOKES</span></h1>
+          <img className='emoji' src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt='Laugh' />
+          <button onClick={this.handleClick} className='button'><h1>New Jokes</h1></button>
+        </div>
+        <div className="jokes">
+          {jokesRender}
+        </div>
+      </div>
+    );
+    
+    return this.state.loading ? loader : application;
+  }
 }
 
 export default Jokes;
