8c260379f3dba09c0434da8dfeb2fedd71f266db
Andres2D <andres2d1997.1206@gmail.com>
2022-05-11 08:18:32 -0500

    Complete real routing component

--- react-router/src/real-routing/Cat.js
+++ react-router/src/real-routing/Cat.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class Cat extends Component {
+
+  componentDidMount(){
+    console.log('componentDidMount');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
   render() {
+    console.log('render');
     return(
       <div>
         <h1>Cat</h1>
+        <h3>This cat is name: {this.props.name}</h3>
         <img style={{width: '180px'}} src='https://i.pinimg.com/originals/46/37/63/4637632aa50dec67576174f269915e11.png' alt='cat' />
       </div>
     )
--- react-router/src/real-routing/Main.js
+++ react-router/src/real-routing/Main.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import {Route, Routes} from 'react-router-dom';
+import {
+  // Link, 
+  NavLink, 
+  Route, 
+  Routes
+} from 'react-router-dom';
+import About from './About';
 import Cat from './Cat';
+import Contact from './Contact';
+import './Main.css';
 
 class Main extends Component {
   render() {
     return (
       <div>
+        <nav className="App-nav">
+          {/* <Link to='/'>About</Link>
+          <Link to='/cat'>Cat</Link>
+          <Link to='/contact'>Contact</Link> */}
+          <NavLink className={(navData)=>navData.isActive?"active-link":""} to='/'>About</NavLink>
+          <NavLink className={(navData)=>navData.isActive?"active-link":""} to='/cat'>Cat</NavLink>
+          <NavLink className={(navData)=>navData.isActive?"active-link":""} to='/contact'>Contact</NavLink>
+        </nav>
         <Routes>
-          <Route path='/cat' element={<Cat />} />
+          <Route path='/' element={<About />} />
+          <Route path='/cat' element={<Cat name='Lilith' />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </div>
      )
